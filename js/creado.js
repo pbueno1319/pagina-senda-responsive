b2cde9b45c1765e1f96c5e8a6b5bdeb1bb95fce9
Pablo123457 <pablobuenoromero@gmail.com>
2018-09-05 11:05:50 -0500

    primer commit

---
+++ js/creado.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var options{
+        inDuration: 400
+    }
+    var instances = M.Materialbox.init(elems, options);
+  });
